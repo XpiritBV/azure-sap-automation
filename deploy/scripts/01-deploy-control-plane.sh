afdcf5240c4f93819fec4c0eec218b054ecb8471
Robert de Veen <rdeveen@xpirit.com>
2024-02-12 20:46:14 +0100

    Add Terraform installation step

--- deploy/scripts/01-deploy-control-plane.sh
+++ deploy/scripts/01-deploy-control-plane.sh
@@ -130,16 +130,17 @@
       echo -e "$green --- Install zip ---$reset"
       sudo apt -qq install zip
 
-      tf_url="https://releases.hashicorp.com/terraform/${tf_version}/terraform_${tf_version}_linux_amd64.zip"
+    #   tf_url="https://releases.hashicorp.com/terraform/${tf_version}/terraform_${tf_version}_linux_amd64.zip"
 
-      echo -e "$green --- Install terraform ${tf_version} from ${tf_url} ---$reset"
-      wget -q ${tf_url}
-      return_code=$?
-      if [ 0 != $return_code ]; then
-          exit_error "Unable to download Terraform version ${tf_version}." 2
-      fi
-      unzip -qq terraform_${tf_version}_linux_amd64.zip ; sudo mv terraform /bin/
-      rm -f terraform_${tf_version}_linux_amd64.zip 
+    #   echo -e "$green --- Install terraform ${tf_version} from ${tf_url} ---$reset"
+    #   wget -q ${tf_url}
+    #   return_code=$?
+    #   if [ 0 != $return_code ]; then
+    #       exit_error "Unable to download Terraform version ${tf_version}." 2
+    #   fi
+    #   unzip -qq terraform_${tf_version}_linux_amd64.zip ; sudo mv terraform /bin/
+    #   rm -f terraform_${tf_version}_linux_amd64.zip 
+      terraform --version
 
       az extension add --name storage-blob-preview >/dev/null
   fi
@@ -151,7 +152,7 @@
       deployer_environment_file_name=$CONFIG_REPO_PATH/.sap_deployment_automation/${ENVIRONMENT}$LOCATION
   end_group
   start_group "Deployment"
-  echo -e "$green--- az login ---$reset"
+      echo -e "$green--- az login ---$reset"
       az login --service-principal --username $ARM_CLIENT_ID --password=$ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID --output none
       return_code=$?
       if [ 0 != $return_code ]; then
