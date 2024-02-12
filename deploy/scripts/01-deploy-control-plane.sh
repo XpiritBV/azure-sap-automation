d88b75e71ae1bc86fe3e9e395c7ee5651441f087
Robert de Veen <rdeveen@xpirit.com>
2024-02-12 20:20:39 +0100

    Add HashiCorp repository and update Terraform version

--- deploy/scripts/01-deploy-control-plane.sh
+++ deploy/scripts/01-deploy-control-plane.sh
@@ -138,8 +138,13 @@
       # fi
       # unzip -qq terraform_${tf_version}_linux_amd64.zip ; sudo mv terraform /bin/
       # rm -f terraform_${tf_version}_linux_amd64.zip
+      wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+      gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+      echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+      sudo apt update -qq
+
       sudo apt -qq install terraform=${tf_version}
-      
+
       az extension add --name storage-blob-preview >/dev/null
   fi
   start_group "Configure parameters"
