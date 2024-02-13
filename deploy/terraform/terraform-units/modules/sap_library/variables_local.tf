83c1b41b38580cc2fffaba1057678690d7a650ba
mstepanova-xpirit <mstepanova@xpirit.com>
2024-02-13 14:58:33 +0100

    minor refactoring

--- deploy/terraform/terraform-units/modules/sap_library/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_library/variables_local.tf
@@ -65,6 +65,44 @@ locals {
                                            )
                                          )
 
-
-
+  pipeline_parameters                 = merge(var.deployer.pipeline_parameters != null ? var.deployer.pipeline_parameters : {},
+                                            {
+                                              "Deployer_State_FileName" = {
+                                                label = var.deployer.deployer_parameter_group_name
+                                                value = var.deployer.deployer_parameter_tf_state_filename
+                                              }
+                                              "Deployer_Key_Vault" = {
+                                                label = var.deployer.deployer_parameter_group_name
+                                                value = try(var.deployer_tfstate.deployer_kv_user_name, "")
+                                              }
+                                              "ControlPlaneEnvironment" = {
+                                                label = var.deployer.deployer_parameter_group_name
+                                                value = var.deployer.deployer_parameter_environment
+                                              }
+                                              "ControlPlaneLocation" = {
+                                                label = var.deployer.deployer_parameter_group_name
+                                                value = var.deployer.deployer_parameter_location
+                                              }  
+                                              "Terraform_Remote_Storage_Resource_Group_Name" = {
+                                                label = var.deployer.deployer_parameter_group_name
+                                                value = local.resource_group_name
+                                              }   
+                                              "Terraform_Remote_Storage_Account_Name" = {
+                                                label = var.deployer.deployer_parameter_group_name
+                                                value = local.sa_tfstate_exists ? (
+                                                          split("/", var.storage_account_tfstate.arm_id)[8]) : (
+                                                          length(var.storage_account_tfstate.name) > 0 ? (
+                                                            var.storage_account_tfstate.name) : (
+                                                            var.naming.storageaccount_names.LIBRARY.terraformstate_storageaccount_name
+                                                          )
+                                                        )
+                                              }
+                                              "Terraform_Remote_Storage_Subscription" = {
+                                                label = var.deployer.deployer_parameter_group_name
+                                                value = local.resource_group_exists ? (
+                                                    split("/", data.azurerm_resource_group.library[0].id))[2] : (
+                                                    split("/", azurerm_resource_group.library[0].id)[2]
+                                                  )
+                                              }                                                 
+                                            })
 }
