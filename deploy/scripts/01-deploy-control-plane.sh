1d03d73096fcb9c0f38ad457e5f5fb054345d189
Hindrik Bruinsma <hbruinsma@xs4some.nl>
2024-02-19 13:13:59 +0100

    fixup typo

--- deploy/scripts/01-deploy-control-plane.sh
+++ deploy/scripts/01-deploy-control-plane.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-. $(pwd}/shared_functions.sh
-. $(pwd}/set-colors.sh
+. $(pwd)/shared_functions.sh
+. $(pwd)/set-colors.sh
 
 start_group "Checking required vars and setting up defaults"
 
