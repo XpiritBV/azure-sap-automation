a9fa895453867d3e72416bddbd6d2c0ce48a0f9a
Hindrik Bruinsma <hbruinsma@xs4some.nl>
2024-02-21 17:05:44 +0100

    Use full github uri for org/repo

--- deploy/scripts/platform/github_functions.sh
+++ deploy/scripts/platform/github_functions.sh
@@ -2,7 +2,7 @@
 
 function setup_dependencies() {
     git config --global --add safe.directory ${GITHUB_WORKSPACE}
-    org_url="$(__get_value_from_context_with_key "github.server_url")/$(__get_value_from_context_with_key "github.organization")"
+    org_url="$(__get_value_from_context_with_key "github.server_url")/$(__get_value_from_context_with_key "github.repository")"
 
     echo "TF_VAR_APP_TOKEN=${APP_TOKEN}"
     echo "TF_VAR_RUNNER_GROUP=${RUNNER_GROUP}"
