cb193a048127af44bef6b4dedfd3b1202ebefdb5
Hindrik Bruinsma <hbruinsma@xs4some.nl>
2024-02-29 14:58:57 +0100

    Make sure we remove TF_LOGS when these are still there. Can be the case on GHES intances

--- deploy/automation/01-deploy-control-plane/01-setup-deployer.sh
+++ deploy/automation/01-deploy-control-plane/01-setup-deployer.sh
@@ -217,6 +217,11 @@ if [[ ${use_webapp,,} == "true" ]]; then # ,, = tolowercase
     export TF_VAR_use_webapp=true
 fi
 
+# File could still be present from previous runs, when using self hosted runners
+if [ -f ${CONFIG_REPO_PATH}/.sap_deployment_automation/terraform.log ]; then
+    rm ${CONFIG_REPO_PATH}/.sap_deployment_automation/terraform.log
+fi
+
 touch ${CONFIG_REPO_PATH}/.sap_deployment_automation/terraform.log
 export TF_LOG_PATH=${CONFIG_REPO_PATH}/.sap_deployment_automation/terraform.log
 
