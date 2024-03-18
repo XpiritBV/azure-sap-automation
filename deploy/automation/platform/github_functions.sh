e1a99c503e90925fbc1d2658ca3fca4163963130
Robert de Veen <rdeveen@xpirit.com>
2024-03-18 09:36:34 +0100

    Don't patch the value if it isnt changed.

--- deploy/automation/platform/github_functions.sh
+++ deploy/automation/platform/github_functions.sh
@@ -82,17 +82,6 @@ function __get_repository_id() {
     echo $repository_id
 }
 
-# function __get_environments() {
-#     api_url=$(__get_value_from_context_with_key "api_url")
-#     repository_id=$(__get_repository_id)
-
-#     curl -Ssf \
-#         -H "Accept: application/vnd.github+json" \
-#         -H "Authorization: Bearer ${APP_TOKEN}" \
-#         -H "X-GitHub-Api-Version: 2022-11-28" \
-#         -L "${api_url}/repositories/${repository_id}/environments/${deployerfolder}/variables"
-# }
-
 function __create_environment() {
     return 0
     # Nothing here yet.
@@ -115,27 +104,28 @@ function __get_value_with_key() {
 
 function __set_value_with_key() {
     key=$1
-    value=$2
+    new_value=$2
 
     api_url=$(__get_value_from_context_with_key "api_url")
     repository_id=$(__get_repository_id)
 
-    if [[ -z $(__get_value_with_key ${key}) ]]; then
+    old_value = $(__get_value_with_key ${key})
+    if [[ -z "${old_value}" ]]; then
         curl -Ss -o /dev/null \
             -X POST \
             -H "Accept: application/vnd.github+json" \
             -H "Authorization: Bearer ${APP_TOKEN}" \
             -H "X-GitHub-Api-Version: 2022-11-28" \
             -L "${api_url}/repositories/${repository_id}/environments/${deployerfolder}/variables" \
-            -d "{\"name\":\"${key}\", \"value\":\"${value}\"}"
-    else
+            -d "{\"name\":\"${key}\", \"value\":\"${new_value}\"}"
+    elif [[ "${old_value}" != "${new_value}" ]]; then
         curl -Ss -o /dev/null \
             -X PATCH \
             -H "Accept: application/vnd.github+json" \
             -H "Authorization: Bearer ${APP_TOKEN}" \
             -H "X-GitHub-Api-Version: 2022-11-28" \
             -L "${api_url}/repositories/${repository_id}/environments/${deployerfolder}/variables/${key}" \
-            -d "{\"name\":\"${key}\", \"value\":\"${value}\"}"
+            -d "{\"name\":\"${key}\", \"value\":\"${new_value}\"}"
     fi
 }
 
