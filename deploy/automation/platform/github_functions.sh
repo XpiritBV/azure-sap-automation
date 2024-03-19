911490843a5c8b7b294e80fe865e96c5f1b29b60
Robert de Veen <rdeveen@xpirit.com>
2024-03-19 13:52:01 +0100

    Refactor GitHub context retrieval and check for GitHub Actions

--- deploy/automation/platform/github_functions.sh
+++ deploy/automation/platform/github_functions.sh
@@ -45,9 +45,7 @@ function __get_value_from_context_with_key() {
         exit_error "Cannot get a value by using an empty key"
     fi
 
-    value=$(jq ".${key}" -r /tmp/github_context.json)
-
-    echo $value
+    echo $(eval echo "\$GITHUB_${key^^}")
 }
 
 function commit_changes() {
