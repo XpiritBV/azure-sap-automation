4db6e797103b3482a7d63ca5cf87db056b39241a
Hindrik Bruinsma <hbruinsma@xs4some.nl>
2024-03-25 09:10:08 +0100

    Read variables from environment variables/pipelines or config files using shared methods

--- deploy/automation/shared_functions.sh
+++ deploy/automation/shared_functions.sh
@@ -145,26 +145,28 @@ function validate_key_value() {
 
 function config_value_with_key() {
     key=$1
+    config_file_name={$2:-$deployer_environment_file_name}
 
     if [[ $key == "" ]]; then
         exit_error "The argument cannot be empty, please supply a key to get the value of" 1
     fi
 
-    echo $(cat ${deployer_environment_file_name} | grep "${key}=" -m1 | awk -F'=' '{print $2}' | xargs)
+    echo $(cat ${config_file_name} | grep "${key}=" -m1 | awk -F'=' '{print $2}' | xargs)
 }
 
 function set_config_key_with_value() {
     key=$1
     value=$2
+    config_file_name={$3:-$deployer_environment_file_name}
 
     if [[ $key == "" ]]; then
         exit_error "The argument cannot be empty, please supply a key to set the value of" 1
     fi
 
-    if grep -q "^$key=" "$deployer_environment_file_name"; then
-        sed -i "s/^$key=.*/$key=$value/" "$deployer_environment_file_name"
+    if grep -q "^$key=" "$config_file_name"; then
+        sed -i "s/^$key=.*/$key=$value/" "$config_file_name"
     else
-88       echo "$key=$value" >>"$deployer_environment_file_name"
+       echo "$key=$value" >>"$config_file_name"
     fi
 }
 
